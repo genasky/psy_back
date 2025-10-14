76ae5c8d3e78d735d9aa068f3c4a6b16e6b6c94b
hebondarau <hebondarau@gmail.com>
2025-10-14 14:40:01 +0300

    fix: fixed google callback path

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('google/callback', passport.authenticate('google', { session: false }),
+router.get('/google/callback', passport.authenticate('google', { session: false }),
     (req, res) => {
         const token = generateToken({ id: req.userId });
         res.redirect(`${process.env.CLIENT_URL}/auth?token=${token}`);
