30195f2be3e01c057c59fd429054bbe3d2ea74f8
hebondarau <hebondarau@gmail.com>
2025-10-20 21:48:57 +0300

    fix: changed payload limits

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,8 @@ import "./services/GoogleService";
 connectDB().then(() => initSlots());
 
 const app = express();
-app.use(json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(
     session({
